c0831610c0c6702d8505c67b2b97d2a5bb4fe709
alexpinho <arp2323arp@gmail.com>
2022-05-25 10:46:21 +0200

    interer xAxis numbers

--- script.js
+++ script.js
@@ -71,6 +71,7 @@ d3.dsv(";", dataTable).then(function (data) {
           2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
           2011, 2012, 2013, 2014, 2015,
         ])
+        .tickFormat(d3.format("d"))
     )
     .select(".domain")
     .remove();
